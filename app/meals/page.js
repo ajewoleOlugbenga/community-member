b5f02c38b26cbad3079e793f48e5f850303adf75
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-06-02 14:35:23 +0100

    Remove "Buy me coffee" link from Meals page.

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,9 +5,6 @@ const Meals = () => {
   return (
     <main>
         <h1>Welcome to our meal sharing</h1>
-        <p>
-            <Link href="/meals/share">Buy me coffee</Link>
-        </p>
       <p>
         <Link href="/meals/meal-1">Share your meals</Link>
       </p>
