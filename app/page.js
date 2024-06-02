7bb0c986d29771ab2f7a15ab0db4b15cf6c57d2e
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-06-02 14:34:37 +0100

    "Remove 'Buy me coffee' link from Meals page, add it to Home page"

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
         Time to get started!
       </h1>
       <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Buy me coffee</Link></p>
       <p><Link href="/community">Community</Link></p>
     </main>
   );
