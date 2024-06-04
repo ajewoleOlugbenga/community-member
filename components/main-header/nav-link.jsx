5c8770d95d371d78ea334e2f04f430e0bd390d16
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-06-04 02:17:01 +0100

    feat: create nav-link.jsx

---
+++ components/main-header/nav-link.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import classes from "./nav-link.module.css"
+import { usePathname } from 'next/navigation';
+
+const NavLink = () => {
+    const path = usePathname();
+  return (
+    <>
+    <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals" className={path.startsWith("/meals") ? classes.active : undefined}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community" className={path==="/community" ? classes.active : undefined}>Foodie community</Link>
+            </li>
+          </ul>
+        </nav>
+    </>
+  )
+}
+
+export default NavLink
