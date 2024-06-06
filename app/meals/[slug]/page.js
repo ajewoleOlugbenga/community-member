dd79a4101e121712b2461db80d876d355b09b61e
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-06-06 23:52:49 +0100

    "Updated MealDetailsPage component to display meal details and added getMeal function to lib/meals.js"

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,12 +1,38 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
+const MealDetailsPage = ({ params }) => {
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
-const Share = () => {
+  if (!meal) {
+    notFound();
+  }
   return (
-    <main>
-      <p>Meals</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 };
 
-export default Share;
+export default MealDetailsPage;
