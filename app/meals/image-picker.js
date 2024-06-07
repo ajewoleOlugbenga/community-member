81e5f4336efa6f87d351870dade5fd364ddde321
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-06-07 11:16:14 +0100

    "Updated ImagePicker component to use React useRef and added button to trigger file input click"

--- app/meals/image-picker.js
+++ app/meals/image-picker.js
@@ -1,12 +1,33 @@
-import classes from "./image-picker.modules.css"
+"use client";
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
 
-export default function ImagePicker({label,name}) {
+export default function ImagePicker({ label, name }) {
+  const ImagePicker = useRef();
+
+  const handlePickClick = () => {
+    ImagePicker.current.click();
+  };
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
-        <input type="file" id={name} accept="image/png, image/jpeg" name={name} />
+        <input
+          className={classes.picker}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={ImagePicker}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
       </div>
     </div>
-  )
+  );
 }
